fd52e297e0e3f4efc2eb99b21ad597d0294cec29
Ryan Francis <ryan.p.francis@gmail.com>
2016-02-15 13:09:19 -0600

    use generators version of decanter gem for testing

--- app/decanters/trip_decanter.rb
+++ app/decanters/trip_decanter.rb
@@ -2,5 +2,5 @@ class TripDecanter < Decanter::Base
   input :name, :string
   input :start_date, :date
   input :end_date, :date
-  has_many :destinations, decanter: AlternativeDestinationDecanter
+  has_many :destinations
 end
