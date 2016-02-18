17d595aed57e7f6cf1b33a02feaf86c05cb3f255
Ryan Francis <ryan.p.francis@gmail.com>
2016-02-17 18:05:41 -0600

    delete routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,3 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  resources :trips
-  namespace :basic_example do
-    resources :trips
-    resources :trips_no_decanter
-  end
-  namespace :nested_example do
-    resources :trips
-    resources :trips_no_decanter
-  end
-  namespace :no_strong_params_example do
-    resources :trips
-  end
 end
